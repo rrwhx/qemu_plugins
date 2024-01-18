ec88dc177103abefb8dfb73a152ddcab64144829
Xinyu Li <lixinyu@loongson.cn>
2024-01-18 21:51:39 +0800

    log syscall
    Signed-off-by: Xinyu Li <lixinyu@loongson.cn>

--- bbv2.cc
+++ bbv2.cc
@@ -32,6 +32,7 @@ static std::string bench_name;
 static FILE* bbv_file;
 static FILE* pc_info_file;
 static FILE* log_file;
+static FILE* syscall_file;
 
 uint64_t icount;
 uint64_t bbcount;
@@ -44,7 +45,11 @@ static void plugin_init(const qemu_info_t* info)
     bbv_file = fopen_nofail((bench_name + "/bbv").c_str(), "w");
     pc_info_file = fopen_nofail((bench_name + "/pc_info.txt").c_str(), "w");
     log_file = fopen_nofail((bench_name + "/log.txt").c_str(), "w");
+    syscall_file = fopen_nofail((bench_name + "/syscall.txt").c_str(), "w");
     inst_end = interval_size;
+
+    fprintf(log_file, "target_arch:%s\n",info->target_name);
+    fflush(log_file);
 }
 
 void plugin_exit(qemu_plugin_id_t id, void *p)
@@ -54,6 +59,7 @@ void plugin_exit(qemu_plugin_id_t id, void *p)
     fprintf(log_file, "icount:%ld\n",icount);
     fprintf(log_file, "tb_count:%ld\n",unique_trans_id);
     fclose(log_file);
+    fclose(syscall_file);
     char buf[1024];
     sprintf(buf, "%ld\n", icount);
     qemu_plugin_outs(buf);
@@ -107,6 +113,20 @@ static void tb_record(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
     qemu_plugin_register_vcpu_tb_exec_cb(tb, tb_exec, QEMU_PLUGIN_CB_NO_REGS, udata);
 }
 
+static void vcpu_syscall(qemu_plugin_id_t id, unsigned int vcpu_index,
+                         int64_t num, uint64_t a1, uint64_t a2,
+                         uint64_t a3, uint64_t a4, uint64_t a5,
+                         uint64_t a6, uint64_t a7, uint64_t a8)
+{
+    fprintf(syscall_file, "icount:%ld, syscall #%" PRIi64 "\n", icount, num);
+}
+
+static void vcpu_syscall_ret(qemu_plugin_id_t id, unsigned int vcpu_idx,
+                             int64_t num, int64_t ret)
+{
+    fprintf(syscall_file, "icount:%ld, syscall #%" PRIi64 " returned -> %" PRIi64 "\n", icount, num, ret);
+}
+
 QEMU_PLUGIN_EXPORT
 int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info,
                         int argc, char **argv)
@@ -124,5 +144,7 @@ int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info,
 
     qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record);
     qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
+    qemu_plugin_register_vcpu_syscall_cb(id, vcpu_syscall);
+    qemu_plugin_register_vcpu_syscall_ret_cb(id, vcpu_syscall_ret);
     return 0;
 }
