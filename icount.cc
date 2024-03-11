891c817883788b7c31bfc34068a22bfac99831e2
Xinyu Li <lixinyu@loongson.cn>
2024-03-11 16:48:20 +0800

    update icount.cc support option icount=[insn_inline|insn_cb|tb_inline|tb_cb]

--- icount.cc
+++ icount.cc
@@ -27,8 +27,33 @@ void plugin_exit(qemu_plugin_id_t id, void *p)
     qemu_plugin_outs(buf);
 }
 
-static void tb_record(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
-{
+static void vcpu_insn_exec(unsigned int vcpu_index, void* userdata) {
+    ++ icount;
+}
+
+static void vcpu_tb_exec(unsigned int vcpu_index, void* userdata) {
+    icount += (intptr_t)userdata;
+}
+
+static void tb_record_insn_cb(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
+    size_t insns = qemu_plugin_tb_n_insns(tb);
+    for (size_t i = 0; i < insns; i ++) {
+        struct qemu_plugin_insn *insn = qemu_plugin_tb_get_insn(tb, i);
+        qemu_plugin_register_vcpu_insn_exec_cb(insn, vcpu_insn_exec, QEMU_PLUGIN_CB_NO_REGS, NULL);
+    }
+}
+
+static void tb_record_insn_inline(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
+    size_t insns = qemu_plugin_tb_n_insns(tb);
+    for (size_t i = 0; i < insns; i ++) {
+        qemu_plugin_register_vcpu_tb_exec_inline(tb,QEMU_PLUGIN_INLINE_ADD_U64, (void*)&icount, 1);
+    }
+}
+
+static void tb_record_tb_cb(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
+    qemu_plugin_register_vcpu_tb_exec_cb(tb, vcpu_tb_exec, QEMU_PLUGIN_CB_NO_REGS, (void*)qemu_plugin_tb_n_insns(tb));
+}
+static void tb_record_tb_inline(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
     qemu_plugin_register_vcpu_tb_exec_inline(tb,QEMU_PLUGIN_INLINE_ADD_U64, (void*)&icount, qemu_plugin_tb_n_insns(tb));
 }
 
@@ -36,7 +61,30 @@ QEMU_PLUGIN_EXPORT
 int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info,
                         int argc, char **argv)
 {
-    qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record);
+    if (!argc) {
+        qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record_tb_inline);
+    }
+    for (int i = 0; i < argc; i++) {
+        qemu_plugin_outs(argv[i]);
+        qemu_plugin_outs("\n");
+        char *p = strchr(argv[i], '=');
+        if (p) {
+            if (strncmp(argv[i], "icount", p - argv[i]) == 0){
+                ++p;
+                if        (strcmp(p, "insn_inline")  == 0) { qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record_insn_inline);
+                } else if (strcmp(p, "insn_cb")      == 0) { qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record_insn_cb);
+                } else if (strcmp(p, "tb_inline")    == 0) { qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record_tb_inline);
+                } else if (strcmp(p, "tb_cb")        == 0) { qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record_tb_cb);
+                } else {
+                    qemu_plugin_outs("unsupported icount ");
+                    qemu_plugin_outs(p);
+                    qemu_plugin_outs("\n");
+                    qemu_plugin_outs("icount=[insn_inline|insn_cb|tb_inline|tb_cb]");
+                    exit(0);
+                }
+            }
+        }
+    }
     qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
     return 0;
 }
