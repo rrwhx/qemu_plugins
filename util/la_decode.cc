cbfb29c95fc0783f4bb47fefa3e4cfd59968b7dd
Xinyu Li <lixinyu@loongson.cn>
2024-08-21 17:52:52 +0800

    generic la_decode.cc

--- util/la_decode.cc
+++ util/la_decode.cc
@@ -1,8 +1,13 @@
 #include "../loongarch_decode_insns.c.inc"
 
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char** argv) {
     uint32_t a = 0x02bffc0d;
+
+    if (argc > 1) {
+        a = strtol(argv[1], NULL, 0);
+    }
 
     LA_DECODE la_decode;
     decode(&la_decode, a);
